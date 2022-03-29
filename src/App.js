e27d8a321176a31cea73b1995e0f5e637e341e2d
Brian Yoon <infobyoon@gmail.com>
2022-03-29 13:53:28 -0700

    Scaffolding components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Form from './Form';
+import Footer from './Footer';
+
+function App() {
+	return (
+		<div className='App'>
+      <Header></Header>
+			<Form></Form>
+      <Footer></Footer>
+		</div>
+	);
+}
+
+export default App;
